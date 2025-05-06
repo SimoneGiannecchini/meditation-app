813553c5bbcdc7b564b05b4388717f6a2cbc5ae5
Simone <giannecchini10@gmail.com>
2025-05-06 18:38:08 +0200

    modifiche bottone apportate

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -15,10 +15,9 @@ const About = () => {
         We aim to promote a culture of awareness and serenity, where meditation
         becomes a daily tool for living better and with greater presence.
       </p>
-      <Link to="/meditation"
-       className="zen-button">
-       Start Meditating
-      </Link>
+      <Link to="/meditation">
+             <button className="zen-button">Start now</button>
+           </Link>
     </div>
   );
 };
