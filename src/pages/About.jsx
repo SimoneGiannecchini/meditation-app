60d0641878b4bb655a41dea8355f019f06c53f3b
Simone <giannecchini10@gmail.com>
2025-05-06 19:01:36 +0200

    sistemato About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "../styles.scss";
 
 const About = () => {
+  const navigate = useNavigate();
+
+  const handleClick = (e) => {
+    e.target.classList.add("clicked");
+    setTimeout(() => {
+      navigate("/meditation");
+    }, 200);
+  };
+
   return (
     <div className="page-about">
       <h1>Our Mission</h1>
@@ -10,14 +19,16 @@ const About = () => {
         To offer a digital space where everyone can rediscover calm, balance,
         and inner well-being through accessible meditation experiences.
       </p>
+
       <h2>Our Vision</h2>
       <p>
         We aim to promote a culture of awareness and serenity, where meditation
         becomes a daily tool for living better and with greater presence.
       </p>
-      <Link to="/meditation">
-             <button className="zen-button">Start now</button>
-           </Link>
+
+      <button className="zen-button" onClick={handleClick}>
+        Start now
+      </button>
     </div>
   );
 };
